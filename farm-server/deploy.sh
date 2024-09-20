fd0fa68109f40e79b84a848776d30023fd6c5083
phan <hanfeelhoo@naver.com>
2024-09-20 12:28:01 +0900

    [feat]: package & install shell script 추가

---
+++ farm-server/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TARGET_DEVICE="rbpi4"
+
+SERVER_WEB_APP="farm_control_dashboard"
+SERVICE_DIR="service"
+SERVICES=("service_plant_measure" "service_proxy_camera" "service_sensor" "service_socket-server")
+
+PACKAGE_TARGET=$SERVER_WEB_APP
+for service in "${SERVICES[@]}"; do
+  PACKAGE_TARGET+=" ${SERVICE_DIR}/${service}"
+done
+
+# 색상 코드 정의
+GREEN='\033[0;32m'
+NC='\033[0m' # 색상 초기화
+
+echo "==============================="
+printf "${GREEN}Ares Package Information${NC}\n"
+echo "==============================="
+printf "ares-package is running: ${GREEN}%s${NC}\n" "$SERVER_WEB_APP"
+for service in "${SERVICES[@]}"; do
+  printf "ares-package is running: ${GREEN}%s${NC}\n" "${SERVICE_DIR}/${service}"
+done
+echo "==============================="
+ares-package $PACKAGE_TARGET
+
+# .ipk 파일 검색 (가장 최근에 수정된 파일)
+INSTALL_IPK_FILE=$(ls -t ./*.ipk 2>/dev/null | head -1)
+
+# 파일이 있는지 확인
+if [ -z "$INSTALL_IPK_FILE" ]; then
+  echo "Error: .ipk 파일을 찾을 수 없습니다."
+  exit 1
+else
+  echo "Installing: $INSTALL_IPK_FILE"
+  ares-install "$INSTALL_IPK_FILE" -d $TARGET_DEVICE -v
+fi
