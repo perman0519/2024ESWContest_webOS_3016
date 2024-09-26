70ba73fae25d07c6578380a6f389d3bfb1e26e5e
phan <hanfeelhoo@naver.com>
2024-09-26 22:07:04 +0900

    [feat] : 각 서버들 서비스 유지를 위한 heartbeat 추가

--- farm-server/service/service_proxy_camera/service_main.js
+++ farm-server/service/service_proxy_camera/service_main.js
@@ -32,21 +32,20 @@ const { startHttpServer } = require('./http-proxy-server.js');
 service.register("startWSServer", (msg) => {
     startWSServer();
 
-    // //heartbeat 구독
     // //------------------------- heartbeat 구독 -------------------------
-    // const sub = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, {subscribe: true});
-    // const max = 10000; //heart beat 횟수 /// heart beat가 꺼지면, 5초 정도 딜레이 생김 --> 따라서 이 녀석도 heart beat를 무한히 돌릴 필요가 있어보임.
-    // let count = 0;
-    // sub.addListener("response", function(msg) {
-    //     console.log(JSON.stringify(msg.payload));
-    //     if (++count >= max) {
-    //         sub.cancel();
-    //         setTimeout(function(){
-    //             console.log(max+" responses received, exiting...");
-    //             process.exit(0);
-    //         }, 1000);
-    //     }
-    // });
+    const sub = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, {subscribe: true});
+    const max = 5000; //heart beat 횟수 /// heart beat가 꺼지면, 5초 정도 딜레이 생김 --> 따라서 이 녀석도 heart beat를 무한히 돌릴 필요가 있어보임.
+    let count = 0;
+    sub.addListener("response", function(msg) {
+        console.log(JSON.stringify(msg.payload));
+        if (++count >= max) {
+            sub.cancel();
+            setTimeout(function(){
+                console.log(max+" responses received, exiting...");
+                process.exit(0);
+            }, 1000);
+        }
+    });
 
     msg.respond({
         returnValue: true,
@@ -57,20 +56,20 @@ service.register("startWSServer", (msg) => {
 service.register("startHttpServer", (msg) => {
     startHttpServer();
 
-    //  //------------------------- heartbeat 구독 -------------------------
-    //  const sub = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, {subscribe: true});
-    //  const max = 10000; //heart beat 횟수 /// heart beat가 꺼지면, 5초 정도 딜레이 생김 --> 따라서 이 녀석도 heart beat를 무한히 돌릴 필요가 있어보임.
-    //  let count = 0;
-    //  sub.addListener("response", function(msg) {
-    //      console.log(JSON.stringify(msg.payload));
-    //      if (++count >= max) {
-    //          sub.cancel();
-    //          setTimeout(function(){
-    //              console.log(max+" responses received, exiting...");
-    //              process.exit(0);
-    //          }, 1000);
-    //      }
-    //  });
+     //------------------------- heartbeat 구독 -------------------------
+     const sub = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, {subscribe: true});
+     const max = 5000; //heart beat 횟수 /// heart beat가 꺼지면, 5초 정도 딜레이 생김 --> 따라서 이 녀석도 heart beat를 무한히 돌릴 필요가 있어보임.
+     let count = 0;
+     sub.addListener("response", function(msg) {
+         console.log(JSON.stringify(msg.payload));
+         if (++count >= max) {
+             sub.cancel();
+             setTimeout(function(){
+                 console.log(max+" responses received, exiting...");
+                 process.exit(0);
+             }, 1000);
+         }
+     });
 
     msg.respond({
         returnValue: true,
@@ -80,52 +79,52 @@ service.register("startHttpServer", (msg) => {
 
 // //----------------------------------------------------------------------heartbeat----------------------------------------------------------------------
 // // handle subscription requests
-// const subscriptions = {};
-// let heartbeatinterval;
-// let x = 1;
-// function createHeartBeatInterval() {
-//     if (heartbeatinterval) {
-//         return;
-//     }
-//     console.log(logHeader, "create_heartbeatinterval");
-//     heartbeatinterval = setInterval(function() {
-//         sendResponses();
-//     }, 1000);
-// }
+const subscriptions = {};
+let heartbeatinterval;
+let x = 1;
+function createHeartBeatInterval() {
+    if (heartbeatinterval) {
+        return;
+    }
+    console.log(logHeader, "create_heartbeatinterval");
+    heartbeatinterval = setInterval(function() {
+        sendResponses();
+    }, 1000);
+}
 
 // send responses to each subscribed client
-// function sendResponses() {
-//     console.log(logHeader, "send_response");
-//     console.log("Sending responses, subscription count=" + Object.keys(subscriptions).length);
-//     for (const i in subscriptions) {
-//         if (Object.prototype.hasOwnProperty.call(subscriptions, i)) {
-//             const s = subscriptions[i];
-//             s.respond({
-//                 returnValue: true,
-//                 event: "beat " + x
-//             });
-//         }
-//     }
-//     x++;
-// }
+function sendResponses() {
+    console.log(logHeader, "send_response");
+    console.log("Sending responses, subscription count=" + Object.keys(subscriptions).length);
+    for (const i in subscriptions) {
+        if (Object.prototype.hasOwnProperty.call(subscriptions, i)) {
+            const s = subscriptions[i];
+            s.respond({
+                returnValue: true,
+                event: "beat " + x
+            });
+        }
+    }
+    x++;
+}
 
-// var heartbeat = service.register("heartbeat");
-// heartbeat.on("request", function(message) {
-//     console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat");
-//     message.respond({event: "beat"}); // initial response
-//     if (message.isSubscription) {
-//         subscriptions[message.uniqueToken] = message; //add message to "subscriptions"
-//         if (!heartbeatinterval) {
-//             createHeartBeatInterval();
-//         }
-//     }
-// });
-// heartbeat.on("cancel", function(message) {
-//     delete subscriptions[message.uniqueToken]; // remove message from "subscriptions"
-//     var keys = Object.keys(subscriptions);
-//     if (keys.length === 0) { // count the remaining subscriptions
-//         console.log("no more subscriptions, canceling interval");
-//         clearInterval(heartbeatinterval);
-//         heartbeatinterval = undefined;
-//     }
-// });
+var heartbeat = service.register("heartbeat");
+heartbeat.on("request", function(message) {
+    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat");
+    message.respond({event: "beat"}); // initial response
+    if (message.isSubscription) {
+        subscriptions[message.uniqueToken] = message; //add message to "subscriptions"
+        if (!heartbeatinterval) {
+            createHeartBeatInterval();
+        }
+    }
+});
+heartbeat.on("cancel", function(message) {
+    delete subscriptions[message.uniqueToken]; // remove message from "subscriptions"
+    var keys = Object.keys(subscriptions);
+    if (keys.length === 0) { // count the remaining subscriptions
+        console.log("no more subscriptions, canceling interval");
+        clearInterval(heartbeatinterval);
+        heartbeatinterval = undefined;
+    }
+});
