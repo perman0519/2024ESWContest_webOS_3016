21e1a9a9da48c9f64da0bcb1796677d32c07ca29
junssong <xms2007@naver.com>
2024-09-26 15:35:00 +0900

    feat: npm_install.sh

---
+++ npm_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# cd client/clientApp
+
+npm install  --prefix ./client/clientApp
+# npm install --prefix ./farm-server/service/ervice_plant_measure
+for d in ./farm-server/service/*; do
+	if [ -d "$d" ]; then
+		npm install --prefix $d
+	fi
+done
