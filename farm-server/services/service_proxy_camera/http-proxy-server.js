784882cd069face3e64648915eb44535249b07fc
junssong <xms2007@naver.com>
2024-10-04 13:44:29 +0900

    feat: timelapse api

--- farm-server/services/service_proxy_camera/http-proxy-server.js
+++ farm-server/services/service_proxy_camera/http-proxy-server.js
@@ -1,7 +1,12 @@
+const { database }  = require('./firebase.js');
+const { onValue } = require('firebase/database');
+const databaseRef = require('firebase/database').ref;
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = 8081;
 
 // Placeholder image path
@@ -13,6 +18,65 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
+});
+
+app.get('/timelapse', (req, res) => {
+    const imageList = fs.readdirSync('/media/multimedia/sector/0');
+    res.setHeader('Content-Type', 'multipart/x-mixed-replace; boundary=frame');
+
+    const sendImage = (imagePath) => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(imagePath, (err, imageBytes) => {
+                if (err) {
+                    console.error('Error reading image file:', err);
+                    const placeholderImagePath = path.join(__dirname, 'placeholder.jpg'); // 플레이스홀더 이미지 경로 설정
+                    fs.readFile(placeholderImagePath, (placeholderErr, placeholderBytes) => {
+                        if (placeholderErr) {
+                            console.error('Error reading placeholder file:', placeholderErr);
+                            reject(placeholderErr);
+                            return;
+                        }
+                        res.write('--frame\r\n');
+                        res.write('Content-Type: image/jpeg\r\n\r\n');
+                        res.write(placeholderBytes);
+                        res.write('\r\n');
+                        resolve();
+                    });
+                } else {
+                    res.write('--frame\r\n');
+                    res.write('Content-Type: image/jpeg\r\n\r\n');
+                    res.write(imageBytes);
+                    res.write('\r\n');
+                    resolve();
+                }
+            });
+        });
+    };
+
+    const streamImages = async () => {
+        for (const image of imageList) {
+            console.log('Sending image:', image);
+            await sendImage('/media/multimedia/sector/0/' + image);
+            await new Promise(resolve => setTimeout(resolve, 50)); // 0.5초 대기
+        }
+        res.end(); // 모든 이미지 전송 후 연결 종료
+    };
+
+    streamImages().catch(error => {
+        console.error('Error in streamImages:', error);
+        res.end(); // 오류 발생 시 연결 종료
+    });
+});
+
+app.get('/api/sensor', (req, res) => {
+    const sensor = databaseRef(database, 'sector/0/sensorData');
+    onValue(sensor, (snapshot) => {
+      const data = snapshot.val();
+      res.json(data);
+    }, (error) => {
+      console.error('Error fetching sensor data:', error);
+      res.status(500).json({ error: 'Failed to fetch sensor data' });
+    });
 });
 
 app.get('/stream', (req, res) => {
@@ -59,4 +123,5 @@ const startHttpServer = () => {
     });
 }
 
+// startHttpServer();
 module.exports = { startHttpServer };
--- farm-server/services/service_proxy_camera/service_main.js
+++ farm-server/services/service_proxy_camera/service_main.js
@@ -1,12 +1,13 @@
 // eslint-disable-next-line import/no-unresolved
 const pkgInfo = require('./package.json');
 const Service = require('webos-service');
-
+const fs = require('fs');
 const service = new Service(pkgInfo.name); // Create service by service name on package.json
 const logHeader = "[" + pkgInfo.name + "]";
 
 const { startWSServer } = require('./ws-server.js');
 const { startHttpServer } = require('./http-proxy-server.js');
+const { saveLocal } = require('./save_local.js');
 
 service.register("startWSServer", (msg) => {
     startWSServer();
@@ -56,8 +57,95 @@ service.register("startHttpServer", (msg) => {
     });
 });
 
+service.register("saveLocal", async (message) => {
+    try {
+        const img = await saveLocal();
+        console.log("img name: ", img);
+        if (img)
+            saveImage(img, "0");
+        setInterval(async () => {
+            const img = await saveLocal();
+            console.log("img name: ", img);
+            if (img)
+                saveImage(img, "0");
+        }, 10000);
+
+            //------------------------- heartbeat 구독 -------------------------
+        const sub = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, {subscribe: true});
+        const max = 5000; //heart beat 횟수 /// heart beat가 꺼지면, 5초 정도 딜레이 생김 --> 따라서 이 녀석도 heart beat를 무한히 돌릴 필요가 있어보임.
+        let count = 0;
+        sub.addListener("response", function(msg) {
+            console.log(JSON.stringify(msg.payload));
+            if (++count >= max) {
+                sub.cancel();
+                setTimeout(function(){
+                    console.log(max+" responses received, exiting...");
+                    process.exit(0);
+                }, 1000);
+            }
+        });
+        message.respond({
+            returnValue: true,
+            Response: "Image stored serive started"
+        });
+    } catch (error) {
+        console.error("Error: ", error);
+        message.respond({
+            returnValue: false,
+            Response: `${error}`
+        });
+    }
+});
+
+service.register("startAll", (msg) => {
+    try {
+        service.call("luna://com.farm.server.camera.service/startWSServer", {}, (response) => {
+            console.log("Call to startWSServer");
+            console.log("Message payload:", JSON.stringify(response.payload));
+        });
+        service.call("luna://com.farm.server.camera.service/startHttpServer", {}, (response) => {
+            console.log("Call to startHttpServer");
+            console.log("Message payload:", JSON.stringify(response.payload));
+        });
+        service.call("luna://com.farm.server.camera.service/saveLocal", {}, (response) => {
+            console.log("Call to saveLocal");
+            console.log("Message payload:", JSON.stringify(response.payload));
+        });
+    } catch (err) {
+        msg.respond({
+            returnValue: false,
+            Response: `${err}`
+        });
+    }
+    msg.respond({
+        returnValue: true,
+        Response: "camera service start all"
+    });
+});
+
+function saveImage(imageName, sector) {
+    var params = {
+        "objects": [
+            {
+                "_kind": "com.farm.server.camera.service:1",
+                "name": imageName,
+                "sector": sector
+            }
+        ]
+    };
+    service.call("luna://com.webos.service.db/put", params, function(response) {
+        if (response.payload.returnValue === true) {
+            console.log("Image saved successfully");
+        } else {
+            console.log("Failed to save image:", JSON.stringify(response.payload));
+			throw new Error("Failed to save image: " +  JSON.stringify(response.payload));
+        }
+    });
+}
+
 // //----------------------------------------------------------------------heartbeat----------------------------------------------------------------------
 // // handle subscription requests
+
 const subscriptions = {};
 let heartbeatinterval;
 let x = 1;
@@ -68,7 +156,7 @@ function createHeartBeatInterval() {
     console.log(logHeader, "create_heartbeatinterval");
     heartbeatinterval = setInterval(function() {
         sendResponses();
-    }, 1000);
+    }, 5000);
 }
 
 // send responses to each subscribed client
